fa17d007a48de35d27dd96182bdcfd974d3fc961
mzaifquraishi <zaifquraishi118@gmail.com>
2021-08-30 21:02:39 +0530

    2 files modified, 1 deleted

--- index.tsx
+++ index.tsx
@@ -1,8 +1,32 @@
+import moment=require('moment');
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { Card } from 'semantic-ui-react';
 import './style.css';
-import Weather from './weather';
+
+const Weather = ({ weatherData }) => (
+  <Card>
+    <Card.Content>
+      <Card.Header className="header">
+        City Name: {weatherData.name}
+      </Card.Header>
+      <p>Day: {moment().format('dddd')}</p>
+      <p>Date: {moment().format('LL')}</p>
+      <p>Temprature: {weatherData.main.temp} &deg;C</p>
+      <p>
+        Sunrise:{' '}
+        {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}
+      </p>
+      <p>
+        Sunset:{' '}
+        {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}
+      </p>
+      <p>Description: {weatherData.weather[0].main}</p>
+      <p>Humidity: {weatherData.main.humidity} %</p>
+    </Card.Content>
+  </Card>
+);
 
 function App() {
   const [lat, setLat] = useState([]);
